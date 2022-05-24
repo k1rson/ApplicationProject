9ac58f12a753e3eecbec4921c70db35239f660fb
MIRON <1111111111aleksey46@gmail.com>
2022-05-24 20:57:32 +0300

    add checkRegUser

--- sqlFuncs.cs
+++ sqlFuncs.cs
@@ -16,11 +16,35 @@ namespace ApplicationProject
             MySqlConnection conn = DB.GetDBConnection();
             conn.Open();
 
+
+
             string sql = $"INSERT INTO users (username, password, role) VALUES ('{username}', '{password}', 'User')";
             MySqlCommand cmd = new MySqlCommand(sql, conn);
             cmd.ExecuteNonQuery();
 
             conn.Close();
+        }
+
+        public static bool ChekUserReg(string username)
+        {
+            MySqlConnection conn = DB.GetDBConnection();
+            conn.Open();
+
+            string sqlCheck = $"SELECT EXISTS(SELECT username FROM users WHERE username = '{username}')";
+
+            MySqlCommand cmd = new MySqlCommand(sqlCheck, conn);
+            MySqlDataReader count = cmd.ExecuteReader();
+            int cont = 0;
+            while (count.Read())
+            {   
+                cont = int.Parse(count[0].ToString());
+            }
+            conn.Close();
+
+            if (cont == 1)
+                return true;
+            else
+                return false;
         }
 
     }
