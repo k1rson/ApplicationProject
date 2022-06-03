513c5d3a53ca9e6349cf3d193f49d9a2792048ac
MIRON <1111111111aleksey46@gmail.com>
2022-06-03 23:05:24 +0300

    edit structure

---
+++ Application c#/config.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationProject
+{
+    public class config
+    {
+        public static string host = "31.31.198.99";
+        public static int port = 3306;
+        public static string database = "u1625777_database";
+        public static string username = "u1625777_shift";
+        public static string password = "cS6sC6wJ3pmD7j";
+
+    }
+}
