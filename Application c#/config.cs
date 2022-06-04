8949674290a7165d4190fa84d6521907d842733b
MIRON <1111111111aleksey46@gmail.com>
2022-06-04 12:30:20 +0300

    edit config

--- Application c#/config.cs
+++ Application c#/config.cs
@@ -8,11 +8,11 @@ namespace ApplicationProject
 {
     public class config
     {
-        public static string host = "31.31.198.99";
+        public static string host = "31.31.198.106";
         public static int port = 3306;
-        public static string database = "u1625777_database";
-        public static string username = "u1625777_shift";
-        public static string password = "cS6sC6wJ3pmD7j";
+        public static string database = "u1698938_database";
+        public static string username = "u1698938_default";
+        public static string password = "KDS82vl6YHCm9tlE";
 
     }
 }
